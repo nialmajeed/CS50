c9df763b7112792e6086a007430f7fcdecd372ce
Nial Majeed <nialmajeed@pbndemonstrator.com>
2026-03-03 07:54:50 +0000

    2 modules completed

--- L2/pagerank/pagerank.py
+++ L2/pagerank/pagerank.py
@@ -40,10 +40,7 @@ def crawl(directory):
 
     # Only include links to other pages in the corpus
     for filename in pages:
-        pages[filename] = set(
-            link for link in pages[filename]
-            if link in pages
-        )
+        pages[filename] = set(link for link in pages[filename] if link in pages)
 
     return pages
 
@@ -57,77 +54,38 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    #create a new directory based on corpus
-    prob_d = {page_name : 0 for page_name in corpus}
-    
-    #links + number of links
+    # create a new directory based on corpus
+    prob_d = {page_name: 0 for page_name in corpus}
+
+    # links + number of links
     links = corpus(page)
     no_links = len(links)
 
-    #We need to identify the number of pages in the corpus 
+    # We need to identify the number of pages in the corpus
     No_pages = len(corpus)
 
-    #IF there are no links - what happens
-    if no_links == 0: 
+    # IF there are no links - what happens
+    if no_links == 0:
         for page_name in prob_d:
             prob_d[page_name] = 1 / len(No_pages)
         return prob_d
-    
 
-    #Probability of picking a random page
-    condition1 = ((1-damping_factor)/(No_pages))
+    # Probability of picking a random page
+    condition1 = (1 - damping_factor) / (No_pages)
 
-  # Poribability of picking a linked page 
-    condition2 =  ((damping_factor)/(No_pages))
+    # Poribability of picking a linked page
+    condition2 = (damping_factor) / (No_pages)
 
     # Retunr probabilities of each one
-    #We first add the probability of conition1 (probability of picking a random page)
+    # We first add the probability of conition1 (probability of picking a random page)
     for page_name in prob_d:
         prob_d[page_name] += condition1
-    
-    #Then we add condition 2 - probability of picking a linked page from said page 
+
+        # Then we add condition 2 - probability of picking a linked page from said page
         if page_name in corpus[page]:
             prob_d[page_name] += condition2
 
     return prob_d
-
-    '''
-      condition = []
-    for link in no_links:
-        iterative = damping_factor/no_links
-        condition.append(iterative)
-    
-    condition2 = damping_factor*sum(condition)
-
-    PR = condition1 + condition2  
-    
-    '''
-
-
-    #we need to provide the probabaility of going to a given linked page given the starting "page" (TODO)
-
-
-    return 
-    
-
-
-    
-   
-'''
-    condition2_set = []
-    for page in corpus: 
-        No_links = 
-        condition2 = (/Nolinks)
-        condition2_set.append(condition2)
-    sum_condition2=damping_factor*sum(condition2_set)
-'''
-        
-
-
-
-    raise NotImplementedError
-    # Example corpus value: {"1.html": {"2.html", "3.html"}, "2.html": {"3.html"}, "3.html": {"2.html"}}
-    #  target output : {"1.html": 0.05, "2.html": 0.475, "3.html": 0.475}
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -139,7 +97,42 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
+    #initial sample 
+    corpus_list = list(corpus.keys())
+    random_page = random.choice(corpus_list)
+
+    #we need a distribution to track how many times each page is visited/sampled
+    samples = {page_name: 0 for page_name in corpus}
+    #add initial visit 
+    samples(random_page)  +=1
+
+    # use random value to hep pick next page based on transition model:
+    random_v = random.random()
+    total_prob = 0
+
+    #To get the next probability after initial 
+    T_model = transition_model(corpus, random_page, damping_factor)
+
+    for page_name, prob in T_model.items():
+        Total_page_prob += prob
+        #if random prob is less than page prob then pick it 
+        if random_v <= total_prob:
+            curr_page = page_name
+            break
+
+    samples(curr_page)  +=1
+
+
+    # Normalisation
+    page_rank = {page_name: (sample_no/n) for page_name, sample_no in samples.items()}
+
+    print('Sum of sample page ranks: ', round(sum(page_rank.values()), 4))
+
+    return page_rank
+
+
     raise NotImplementedError
+
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -151,6 +144,9 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
+
+    #check to ensure no changes 0.001
+
     raise NotImplementedError
 
 
