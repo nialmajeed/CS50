56a34cc18d74330750f126977d45335337b15bed
Nial Majeed <nial.maj@gmail.com>
2026-06-02 17:56:15 +0100

    Updated what was lost best Possible

--- L5/traffic/traffic.py
+++ L5/traffic/traffic.py
@@ -93,18 +93,27 @@ def get_model():
         [
             # Convolutional layer. Learn 32 filters using a 3x3 kernel
             tf.keras.layers.Conv2D(
-                32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+                64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            ),
+            tf.keras.layers.Conv2D(
+                64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            ),
+            tf.keras.layers.Conv2D(
+                64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
             ),
             # Max-pooling layer, using 2x2 pool size
             # tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
             # Max-pooling layer, using 2x2 pool size
-            # tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+            # tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
             # Flatten units
             tf.keras.layers.Flatten(),
             # Add a hidden layer with dropout
-            tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dropout(0.5),
+            # tf.keras.layers.Dense(64, activation="relu"),
+            tf.keras.layers.Dense(256, activation="relu"),
+            # tf.keras.layers.Dense(256, activation="relu"),
+            # Droupout
+            tf.keras.layers.Dropout(0.2),
             # Add an output layer with output units for all 10 digits
             tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"),
         ]
